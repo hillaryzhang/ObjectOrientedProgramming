3f1616e0cfb8ecf20d03ba34a659a2865416742b
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-05 23:56:39 -0800

    implemented skeletal classese for testing

---
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    /// post: Represents one cell in the worksheet
+    public abstract class Cell
+    {
+
+    }
+
+    /// post: Serves as a container for a 2D array of cells. Also serves as a factory for cells (it creates all the cells in the spreadsheet)
+    public class Spreadsheet
+    {
+
+    }
+}
