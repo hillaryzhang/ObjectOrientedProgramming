e7ceeca8526f63221b7618a8358b9273a1424ac4
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-06 03:04:42 -0800

    added RowIndex and ColumnIndex property

--- Spreadsheet_Hillary_Zhang/ClassLibrary1/Class1.cs
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/Class1.cs
@@ -9,7 +9,23 @@ namespace CptS321
     /// post: Represents one cell in the worksheet
     public abstract class Cell
     {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
 
+        protected string text;
+        public string Text { get; }
+        public string Text(string newText)
+        {
+            set
+        }
+        public string Text()
+        {
+            get { return text; }
+            set 
+            {
+                
+            }
+        }
     }
 
     /// post: Serves as a container for a 2D array of cells. Also serves as a factory for cells (it creates all the cells in the spreadsheet)
