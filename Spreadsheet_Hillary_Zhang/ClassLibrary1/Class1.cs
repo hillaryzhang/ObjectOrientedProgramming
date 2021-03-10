02231d12716c918b6a6aa41214f64a8190a7ccb0
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-09 23:24:39 -0800

    implemented property changes in Cell class

--- Spreadsheet_Hillary_Zhang/ClassLibrary1/Class1.cs
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/Class1.cs
@@ -3,30 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CptS321
 {
     /// post: Represents one cell in the worksheet
-    public abstract class Cell
+    public abstract class Cell : INotifyPropertyChanged
     {
+        public delegate void PropertyChangedEventHandler(Object sender, PropertyChangedEventArgs e);
         public int RowIndex { get; set; }
         public int ColumnIndex { get; set; }
 
-        protected string text;
-        public string Text { get; }
-        public string Text(string newText)
-        {
-            set
-        }
-        public string Text()
-        {
-            get { return text; }
-        }
-        public string Text(string newText)
-        {
-            this.text = newText;
+        protected string text = string.Empty;
 
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                if (value == text) { return; }
+                text = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(Text));
+            }
         }
+
+
+
     }
 
     /// post: Serves as a container for a 2D array of cells. Also serves as a factory for cells (it creates all the cells in the spreadsheet)
