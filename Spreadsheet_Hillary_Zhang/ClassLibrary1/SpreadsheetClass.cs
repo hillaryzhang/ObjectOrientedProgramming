5d5163bd6cda79d37736b7ac215f578e26935463
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-12 16:28:41 -0800

    implemented GetCell and properties ColumnCount, RowCount

--- Spreadsheet_Hillary_Zhang/ClassLibrary1/SpreadsheetClass.cs
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/SpreadsheetClass.cs
@@ -17,22 +17,56 @@ namespace CptS321
 
         public event PropertyChangedEventHandler CellPropertyChanged;
 
-        // 5c
-        public Spreadsheet(int rows, int col)
+        /// post: Constructs a spreadsheet that takes a number of rows and columns, and initializes the array of cells with the proper RowIndex and ColumnIndex value
+        public Spreadsheet(int rows, int columns)
         {
             this.rowCount = rows;
-            this.columnCount = col;
-            spreadsheet = new Cell[rows, col];
+            this.columnCount = columns;
+            spreadsheet = new Cell[rows, columns];
 
             for (int r = 1; r <= rows; r++) //to allocate each row
             {
-                for (char c = 'A'; c <= (char)col; c++) //to allocate each column in each row
+                for (int c = 1; c <= columns; c++) //to allocate each column in each row
                 {
                     //5c.2, give array of cells proper RowIndex and ColumnIndex values
+                    spreadsheet[r, c] = new CellImplementation(r, c, "");
+                    spreadsheet[r, c].PropertyChanged += OnPropertyChanged();
 
                 }
             }
 
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            CellPropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        // post: returns the cell at the location of the given row and column index (5g)
+        public Cell GetCell(int rowIndex, int columnIndex)
+        {
+
+            if (rowIndex > spreadsheet.GetLength(0) || columnIndex > spreadsheet.GetLength(1))
+                return null;
+            else
+                return spreadsheet[rowIndex, columnIndex];
+        }
+
+        // post: Returns the number of columns in the spreadsheet (5h)
+        public int ColumnCount
+        {
+            get
+            {
+                return this.columnCount;
+            }
+        }
+
+        // post: Returns the number of rows in the spreadsheet (5h)
+        public int RowCount
+        {
+            get
+            {
+                return this.rowCount;
+            }
         }
     }
 }
