a64c2330c02762b1021e25b8d6402120a6817f01
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-15 22:55:59 -0700

    implemented a helper class for my BinaryOperatorNode

---
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/BinaryOperatorHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CptS321
+{
+    public class BinaryOperatorHelper
+    {
+        internal class AdditionNode : BinaryOperatorNode
+        {
+            public AdditionNode() : base('+')
+            {
+            }
+
+            public override double Evaluate(double left, double right)
+            {
+                return left + right;
+            }
+        }
+
+        internal class SubtractionNode : BinaryOperatorNode
+        {
+            public SubtractionNode() : base('-')
+            {
+            }
+            public override double Evaluate(double left, double right)
+            {
+                return left - right;
+            }
+        }
+
+        internal class MultiplicationNode : BinaryOperatorNode
+        {
+            public MultiplicationNode() : base('*')
+            {
+            }
+            public override double Evaluate(double left, double right)
+            {
+                return left * right;
+            }
+        }
+
+        internal class DivisionNode : BinaryOperatorNode
+        {
+            public DivisionNode() : base('/')
+            {
+            }
+            public override double Evaluate(double left, double right)
+            {
+                return left / right;
+            }
+        }
+    }
+}
