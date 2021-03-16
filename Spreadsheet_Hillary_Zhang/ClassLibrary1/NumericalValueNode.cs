199149d08e316fb7d5578896556d8491dafb7d12
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-15 22:11:25 -0700

    implemented NumericalValueNode class

--- Spreadsheet_Hillary_Zhang/ClassLibrary1/NumericalValueNode.cs
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/NumericalValueNode.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CptS321
 {
     // This class is a node that represents a constant numerical value
     public class NumericalValueNode : BaseNode
     {
-
+       public NumericalValueNode(double number)
+        {
+            this.operatorValue = number;
+            this.name = number.ToString(); //I added
+        }
     }
 }
