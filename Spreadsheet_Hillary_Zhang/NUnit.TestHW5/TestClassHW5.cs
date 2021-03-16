894e51c6d2dc5b2ebfab2ad59c161046961eb6df
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-15 20:05:12 -0700

    created tests for subtraction expressions (TDD)

--- Spreadsheet_Hillary_Zhang/NUnit.TestHW5/TestClassHW5.cs
+++ Spreadsheet_Hillary_Zhang/NUnit.TestHW5/TestClassHW5.cs
@@ -31,5 +31,28 @@ namespace NUnit.TestHW5
         {
             Assert.IsNotNull(this.additionTest);
         }
+
+        private CptS321.ExpressionTree subtractionTest = new CptS321.ExpressionTree("11-5-3");
+
+        // post: makes sure that subtraction yields the correct value based on the given subtraction expression
+        [Test]
+        public void subtractionTestEvaluate()
+        {
+            Assert.AreEqual(3, this.additionTest.Evaluate());
+        }
+
+        // post: makes sure that the given expression matches the final string expression
+        [Test]
+        public void subtractionTestExpression()
+        {
+            Assert.AreEqual("11-5-3", this.additionTest.Expression); // Expression: the string value that is set to the expression
+        }
+
+        // post: makes sure that subtraction does not yield a null
+        [Test]
+        public void subtractionTestIsNotNull()
+        {
+            Assert.IsNotNull(this.additionTest);
+        }
     }
 }
