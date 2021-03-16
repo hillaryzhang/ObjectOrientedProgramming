02bdf40030467d40549b5c4b05fde33b31e71298
Hillary Zhang <hillary.zhang@wsu.edu>
2021-03-15 23:05:36 -0700

    created OperatorNodeFactory class

---
+++ Spreadsheet_Hillary_Zhang/ClassLibrary1/OperatorNodeFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CptS321
+{
+    internal class OperatorNodeFactory
+    {
+        public static BinaryOperatorNode CreateOperatorNode(char theOperator)
+        {
+            switch (theOperator)
+            {
+                case '+':
+                    return new BinaryOperatorHelper.AdditionNode();
+                case '-':
+                    return new BinaryOperatorHelper.SubtractionNode();
+                case '*':
+                    return new BinaryOperatorHelper.MultiplicationNode();
+                case '/':
+                    return new BinaryOperatorHelper.DivisionNode();
+            }
+            return null;
+        }
+
+        public static bool IsValidOperator(char theOperator)
+        {
+            switch (theOperator)
+            {
+                case '-':
+                    return true;
+                case '+':
+                    return true;
+                case '*':
+                    return true;
+                case '/':
+                    return true;
+            }
+            return false;
+        }
+    }
+}
