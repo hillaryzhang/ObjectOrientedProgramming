d5f0039585324fdd5dd94b1b7517ff993e222a81
Hillary Zhang <hillary.zhang@wsu.edu>
2021-04-01 11:59:07 -0700

    setting up testing for HW7 (TDD)

---
+++ Spreadsheet_Hillary_Zhang/NUnit.TestHW5/SpreadsheetTests.cs
@@ -0,0 +1,27 @@
+﻿// Hillary Zhang
+// WSU ID: 11694139
+// 4/2/2021
+// CptS 321
+// Professor: Venera Arnaoudova
+// Assignment 7: Spreadsheet Formula Evaluation
+
+// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NUnit.Test
+{
+    [TestFixture]
+    public class SpreadsheetTests
+    {
+        private CptS321.SpreadsheetClass newTest = new CptS321.SpreadsheetClass(/**/);
+
+        // post: makes sure that 
+        [Test]
+        public void newTest()
+        {
+        }
+    }
+}
